dac0a979dddbc7f4f9c5c03e74a516a3fbcfbdaa
Ruslan <rv.volovik88@gmail.com>
2020-10-24 21:40:56 +0300

    first program, hello world

---
+++ hello.s
@@ -0,0 +1,22 @@
+.text
+
+_start:
+.global  _start
+
+mov $1,   %rax
+mov $1,   %rdi
+mov $msg, %rsi
+mov $len, %rdx
+syscall
+
+call exit
+
+msg:
+  .ascii "hello, world\n"
+
+len = . - msg
+
+exit:
+  mov $60, %rax
+  mov $0, %rdi
+  syscall
