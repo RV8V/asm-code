2b4ef300cc5d937942bd002531d8bb6d3eddcfa6
Ruslan <rv.volovik88@gmail.com>
2020-10-25 19:01:01 +0200

    i/o testing code -> templates

---
+++ input.asm
@@ -0,0 +1,22 @@
+.global _start
+
+.data
+a: .space 10
+
+.text
+_start:
+    mov $0,  %rax
+    mov $0,  %rdi
+    mov $a,  %rsi
+    mov $10, %rdx
+    syscall
+
+    mov $1,  %rax
+    mov $1,  %rdi
+    mov $a,  %rsi
+    mov $10, %rdx
+    syscall
+
+    mov $60, %rax
+    mov $0,  %rsi
+    syscall
