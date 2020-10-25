6d9cae14edad10c39abce459cdb0cf8ce14b6ce6
Ruslan <rv.volovik88@gmail.com>
2020-10-25 19:25:42 +0200

    input testing code -> templates

--- input.asm
+++ input.asm
@@ -5,18 +5,18 @@ a: .space 10
 
 .text
 _start:
-    mov $0,  %rax
-    mov $0,  %rdi
-    mov $a,  %rsi
-    mov $10, %rdx
-    syscall
+	mov $0,  %rax
+    	mov $0,  %rdi
+    	mov $a,  %rsi
+    	mov $10, %rdx
+    	syscall
 
-    mov $1,  %rax
-    mov $1,  %rdi
-    mov $a,  %rsi
-    mov $10, %rdx
-    syscall
+    	mov $1,  %rax
+    	mov $1,  %rdi
+   	 mov $a,  %rsi
+    	mov $10, %rdx
+    	syscall
 
-    mov $60, %rax
-    mov $0,  %rsi
-    syscall
+    	mov $60, %rax
+    	mov $0,  %rsi
+    	syscall
