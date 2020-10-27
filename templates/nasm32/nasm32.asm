b1c273af8dcaa4c91543c2949c1c975d6cfb9124
Ruslan <rv.volovik88@gmail.com>
2020-10-27 19:13:24 +0200

    some changes

--- templates/nasm32/nasm32.asm
+++ templates/nasm32/nasm32.asm
@@ -1,6 +1,9 @@
 segment .data
 	data db "testing code", 0xa, 0xd
 	len equ $ - data
+	message db 'display 9 start', 0xa
+	len_m equ $ - message
+	s2 times 9 db '*'
 
 segment .text
 	global _start
@@ -10,6 +13,12 @@ _start:
 	mov ebx, 1
 	mov ecx, data
 	mov edx, len
+	int 0x80
+	
+	mov edx, 9
+	mov ecx, s2
+	mov ebx, 1
+	mov eax, 4
 	int 0x80
 
 	mov eax, 1
