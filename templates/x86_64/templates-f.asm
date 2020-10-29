2ffa1f81cd56991bee5b262e34a31c34aed6c96b
Ruslan <rv.volovik88@gmail.com>
2020-10-29 14:59:17 +0200

    some changes to file asm -> fasm

--- templates/x86_64/templates-f.asm
+++ templates/x86_64/templates-f.asm
@@ -2,19 +2,25 @@ format ELF64 executable 3
 
 segment readable writable
 	msg db 'message', 10, 0
+	_m  db 'this is another string', 10, 0
 
 segment readable executable
 entry main
 
+include 'io.inc'
+
 main:
 	lea rdi, [msg]
-	call strlen
+	call print
+	lea rdi, [_m]
+	call print
+	; call strlen
 	; mov rax, 9
-	mov rdx, rax
-	mov rsi, rdi
-	mov rdi, 1
-	mov rax, 1
-	syscall
+	; mov rdx, rax
+	; mov rsi, rdi
+	; mov rdi, 1
+	; mov rax, 1
+	; syscall
 
 	xor rdi, rdi
 	mov rax, 60
@@ -34,3 +40,11 @@ strlen:
 	pop  rcx
 	pop  rdi
 	ret
+
+print:
+	call strlen
+	mov rdx, rax
+	mov rsi, rdi
+	mov rdi, 1
+	mov rax, 1
+	syscall
