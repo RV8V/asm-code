4570af8c0b4cf3216b77a8247364f27dd103888d
Ruslan <rv.volovik88@gmail.com>
2021-02-16 11:27:26 +0200

    some test examples

---
+++ masm/TEST.ASM
@@ -0,0 +1,15 @@
+.386
+.model flat, stdcall
+option casemap :none
+include \masm32\include\windows.inc
+include \masm32\include\kernel32.inc
+include \masm32\include\masm32.inc
+includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\masm32.lib
+.data
+ HelloWorld db "Hello World!", 0
+.code
+start:
+ invoke StdOut, addr HelloWorld
+ invoke ExitProcess, 0
+end start 
